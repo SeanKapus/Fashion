e999b17b4c67d842bce9871d613536f2bc16bfaa
Rudy Mutter <rudy@yetihq.com>
2014-08-13 11:33:32 -0700

    Update models.py

--- outfit/models.py
+++ outfit/models.py
@@ -15,10 +15,12 @@ class User(AbstractUser):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     # imageClient = models.ImageField(upload_to='profile_pics/')
 
+    # unicode is supposed to return a unicode string return u"{}".format(self.username)
     def __unicode__(self):
         return self.username
 
 
+# Need to fix the indentation here
 TYPE_CHOICES = (
                 ('T', 'Tops'),
                 ('B', 'Bottoms'),
@@ -30,6 +32,7 @@ TYPE_CHOICES = (
 
 class Clothes(models.Model):
     name = models.CharField(max_length=50)
+    # 'type' is a reserved python keyword and may cause some funky issues
     type = models.CharField(max_length=1, choices=TYPE_CHOICES)
     description = models.CharField(max_length=200, blank=True)
     client = models.ForeignKey(User)
