9c687c0990e9eb3dd4ab9f3c5a1d43d6293a4cd4
Rudy Mutter <rudy@yetihq.com>
2014-08-22 11:27:46 -0700

    Update models.py

--- outfit/models.py
+++ outfit/models.py
@@ -8,7 +8,8 @@ GENDER_CHOICES = (
     ('F', 'Female'),
 )
 
-
+# Probably want to also put null=True where you have blank=True on fields
+# Also probably want to reformat this file, some whitespaces and syntax issues
 class User(AbstractUser):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
@@ -30,6 +31,7 @@ TYPE_CHOICES = (
 
 class Clothes(models.Model):
     name = models.CharField(max_length=50)
+    # type is a reserved keyword in python
     type = models.CharField(max_length=1, choices=TYPE_CHOICES)
     description = models.CharField(max_length=200, blank=True)
     client = models.ForeignKey(User)
@@ -40,6 +42,7 @@ class Clothes(models.Model):
 
 class Favorites(models.Model):
     outfit = models.ManyToManyField(Clothes)
+    # should name this fields user, with a related_name=favs
     favs =models.ForeignKey(User)
 
 
